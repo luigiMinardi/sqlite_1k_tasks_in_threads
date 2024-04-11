ef6c28359be332355c7bb3537b7b31be0546db1f
Luigi <lugminsinlag@gmail.com>
2024-04-11 15:22:56 +0200

    fix: Bash script wasn't installing the dependencies.

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ fi
 source .venv/bin/activate
 echo "Installing missing dependecies if needed."
 pip install --upgrade pip > /dev/null
-pip install -r requirements.txt --dry-run --ignore-installed > /dev/null
+pip install -r requirements.txt --ignore-installed > /dev/null
 ./main.py
